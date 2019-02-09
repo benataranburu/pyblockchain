dcf4c90d7ca8b7cd16afd2201a4d4dd47ebd6a0a
Beñat Aranburu <baranburu@gmail.com>
2019-02-09 10:50:33 +0100

    Basic Blockchain functionality added

--- blockchain.py
+++ blockchain.py
@@ -1,26 +1,32 @@
 import hashlib
 import json
+import requests
 from time import time
-
+from textwrap import dedent
+from uuid import uuid4
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
     def __init__(self):
         self.current_transactions = []
         self.chain = []
-
-        # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "1111"
 
     @staticmethod
     def hash(block):
-        """
-        Create a SHA-256 hash
-
-        :param block: <dict> Block
-        :return: <str>
-        """
-
-        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        # Check dictionary is ordered
         block_str = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_str).hexdigest()
 
@@ -29,14 +35,6 @@ class Blockchain(object):
         return self.chain[-1]
 
     def new_block(self, proof, previous_hash=None):
-        """
-        Create a new block
-
-        :param proof: <int> The proof given by the Proof of Work algorithm
-        :param previous_hash: (Optional) <str> Hash of previous Block
-        :return: <dict> New Block
-        """
-
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time(),
@@ -45,21 +43,13 @@ class Blockchain(object):
             'previous_hash': previous_hash or self.hash(self.chain[-1]),
         }
 
-        # Reset the current list of transactions
         self.current_transactions = []
 
         self.chain.append(block)
         return block
 
     def new_transaction(self, sender, recipient, amount):
-        """
-        Create a new transaction
 
-        :param sender: <str> Address of the Sender
-        :param recipient: <str> Address of the Recipient
-        :param amount: <int> Amount
-        :return: <int> The index of the Block that will hold this transaction
-        """
         self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
@@ -67,3 +57,60 @@ class Blockchain(object):
         })
 
         return self.last_block['index'] + 1
+
+# Instantiate node
+app = Flask(__name__)
+
+# Generate a unique identifier for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    # Check required fields
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    # Create new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    response = {'message': f'Transaction will be added to block {index}'}
+    return jsonify(response), 201
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    # POW algorithm
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Add the new block to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=4444)
